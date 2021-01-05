f985b0760252b032edcfaa71ecc4974c614baa4b
maer01 <mattias.ernelli@comhem.com>
2021-01-05 23:17:04 +0100

    Added SRU mappings, old code reused and added to main ownbox file. deklaration could be a free standing tool but its now incorporated into ownbox main application

--- ownbox.js
+++ ownbox.js
@@ -29,6 +29,7 @@ var options = Object.assign({
   accountingFile: "",
   transactionsFile: "", //"transactions.json",
   baskontoplan: "Kontoplan_Normal_2020.csv",
+  srukoder: "INK2_19-P1-exkl-version-2.csv",
   verifications: "verifications",
   autobook: true,
   noAutobook: false,
@@ -679,6 +680,8 @@ var basKontoplan = {};
 var kontoGrupper = {};
 var kontoKlasser = {};
 
+var SRU_koder = [];
+
 function basKontotyp(kontonr) {
   if (kontonr.startsWith("1")) {
     return 'T';
@@ -702,6 +705,194 @@ function isBalanskonto(kontonr) {
     return false;
   }
 }
+
+function findSRUcode(account) {
+  debug("find SRU code for account: ", account);
+
+  var match = [];
+
+  for(i = 0; i < SRU_koder.length; i++) {
+    //debug("Match account against rule: ", SRU_koder[i].rules);
+    if(account.match(SRU_koder[i].rules.include) && (!SRU_koder[i].rules.exclude || !account.match(SRU_koder[i].rules.exclude))) {
+      //return [SRU_koder[i].code, SRU_koder[i].ink2];
+
+      if(SRU_koder[i].rules.matchsign) {
+	//console.log("Matchsign: " + SRU_koder[i].ink2 + ", " + SRU_koder[i].rules.matchsign);
+	match.push(SRU_koder[i]);
+      } else {
+	return SRU_koder[i];
+      }
+    }
+  }
+
+  if(match.length) {
+    return {
+      srukod: match.map( (a,v) => a.rules.matchsign + ":" + a.srukod).join(","),
+      ink2: match.map( (a,v) => a.rules.matchsign + ":" + a.ink2).join(","),
+      name: match[0].name
+    };
+  }
+}
+
+function importSRUkoder(filename) {
+  var slask = [];
+
+  function parseSRU_Rule(rule) {
+
+    var include = [];
+    var exclude = [];
+    var matchsign;
+
+    debug("parse SRU rule: ", rule);
+
+    rule = rule.trim();
+
+    if(rule.startsWith("+ ")) {
+      matchsign = 1;
+      rule = rule.substring(2);
+    } else if(rule.startsWith("- ") || rule.startsWith('– ')) {
+      matchsign = -1;
+      rule = rule.substring(2);
+    } else {
+      matchsign = 0;
+    }
+
+    //    console.log("process include, exclude for: " + rule);
+
+    // 249x exkl. 2492, 26xx-27xx, 28xx exkl. 286x-287x -> [ "249x exkl. 2492", "249x exkl. 2492", "249x exkl. 2492" ]
+    // 49xx exkl. (4910-4931, 496x, 498x) -> ["49xx exkl. (4910-4931, 496x, 498x)" ]
+
+    // not supported
+    // 49xx exkl. (4910-4931, 496x, 498x), 89xx, 75xx -> ["49xx exkl. (4910-4931, 496x, 498x)", "89xx", "75xx" ]
+
+
+    var parts = [];
+
+    var items = rule.split(/[()]/);
+
+    if(items.length > 1) {
+      // split multi excl rule within parenthesis exkl. (NNxx-MMxx,NNNx, ...)
+
+      if(items.length > 3 || items[2].length > 0) {
+	console.error("Unhandled rule syntax: " + rule);
+	process.exit(1);
+      }
+      // [ '49xx exkl. ', '4910-4931, 496x, 498x', '' ]
+
+      parts = [ items.join(" ").trim() ];
+
+    } else {
+      parts = items[0].split(",");
+    }
+
+    debug("iterate parts: ", parts);
+
+    parts.forEach((part) => {
+      part = part.trim().split("exkl.");
+
+      debug("expand split part: ", part);
+
+      function expand(p) {
+	var res = [];
+
+	debug("expand part: ", p);
+
+	p = p.trim();
+	p = p.split(/[-–]/);
+
+	debug("expand array: ", p);
+
+	if(p.length > 1) {
+	  var from = 1*p[0].match(/\d+/);
+	  var to   = 1*p[1].match(/\d+/);
+
+	  debug("iterate from: ", from, " to ", to);
+
+	  for(i = from; i <= to; i++) {
+	    res.push(((""+i+"xxxx").slice(0,4)).replace(/x/g, "\\d"));
+	  }
+	} else {
+	  res.push(p[0].replace(/x/g, "\\d"));
+	}
+	return res;
+      }
+
+      include = include.concat(expand(part[0]));
+      if(part[1]) {
+	debug("exclude part: ", part);
+	exclude = exclude.concat(expand(part[1]));
+      }
+    });
+
+    debug("include:" + include.join(","));
+    debug("exclude:" + exclude.join(","));
+
+    return {
+      rule: rule,
+      matchsign: matchsign,
+      //	include: include,
+      //	exclude: exclude
+
+      include: include.length ? new RegExp(include.join("|")) : false,
+      exclude: exclude.length ? new RegExp(exclude.join("|")) : false
+    };
+  }
+
+  function addRecord(record) {
+    //console.log("adding SRU codes: ", record);
+
+    if(record[0].match(/^7\d\d\d$/)) {
+      //SRU_koder.push(record[0]);
+
+      //verbose = record[0] === "7251";
+
+      SRU_koder.push({
+	srukod: record[0],
+	ink2: record[1],
+	name: record[2],
+	rules: parseSRU_Rule(record[3])
+      });
+
+    } else {
+      slask.push(record[0]);
+    }
+  }
+
+
+  return new Promise( (resolve, reject) => {
+    var prevLine = "";
+
+    lineReader(fs.createReadStream(filename, { encoding: 'utf8'}).on('end', () => {
+      console.log("srukoder done");
+      console.log("slask: ", slask);
+      //Object.keys(basKontoplan).forEach(k => console.log(basKontoplan[k].kontonr + ": " + basKontoplan[k].kontonamn));
+      return resolve();
+    })).on('line', (line) => {
+      //console.log("got line: " + line);
+
+      if( (line.match(/\"/g) || []).length % 2 === 1) {
+	//console.log("odd quotes: " + line);
+	prevLine += line + '\n';
+	return;
+      }
+
+      if(prevLine.length) {
+	line = prevLine + line;
+	prevLine = "";
+      }
+
+      //var parts = line.split(',').map(p => {
+      //  return p.replace(/"/g, '').trim();
+      //});
+
+
+      var cols = parseCSV2Array(line);
+      addRecord(cols);
+    });
+
+  });
+}
+
 
 // Urval:     ■ \u25a0
 // Ändring:   |  \u2759"
@@ -1412,6 +1603,36 @@ var cmds = {
   },
   baskontoplan: function(filename) {
     importBaskontoplan(filename || options.kontoplan);
+  },
+  srukoder: function(filename) {
+    importSRUkoder(filename || options.srukoder).then( (res) => {
+
+    var testdata = (""+fs.readFileSync("SRU_test.se")).split(/[\r\n]+/);
+
+    console.log("testdata[0] = [" + testdata[0] + "]");
+
+      testdata.forEach(l => {
+	var fields = l.split(/[ ]+/);
+	if(fields[0] === "#SRU") {
+	  var konto = fields[1];
+	  var srukod = fields[2];
+          var res = findSRUcode(konto);
+	  if(res) {
+	    console.log("SRU code for ", konto, srukod, " matches: ", res.srukod); //, JSON.stringify(res));
+	  } else {
+	    console.log("SRU code for KONTO: ", konto, "SRU: ", srukod, " NOMATCH");
+	  }
+	}
+      });
+
+      console.log("test SRU code for 1510: ", findSRUcode("1510"));
+      console.log("test SRU code for 1930: ", findSRUcode("1930"));
+
+
+
+
+    });
+
   },
   autobook: function() {
     console.log("RUN autobook");
